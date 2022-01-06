734efd5d06f969a1e8e5eefb4460d968f7d67072
coltborg <coltborg@gmail.com>
2022-01-05 22:25:17 -0600

    Remove masonry
    - remove imagesLoaded

--- js/app.js
+++ js/app.js
@@ -1,26 +1,5 @@
 (function() {
   var app = {
-    'masonry': {
-      'masonryGridEl': document.querySelector('.o-grid-masonry'),
-      'masonryInit': function() {
-        const masonry = new Masonry( app.masonry.masonryGridEl, {
-          // options
-          columnWidth: '.o-grid-masonry__sizer',
-          itemSelector: '.o-grid-masonry__item',
-          gutter: '.o-grid-masonry__gutter-sizer',
-          percentPosition: true,
-          stagger: 50,
-          transitionDuration: 300,
-          horizontalOrder: true,
-          horizontal: true
-        })
-
-        imagesLoaded(app.masonry.masonryGridEl).on('progress', function() {
-          // layout Masonry after each image loads
-          masonry.layout();
-        });
-      }
-    },
     'getDate': {
       'getYear': function () {
         return new Date().getFullYear();
@@ -33,13 +12,6 @@
       // Copyright Date
       const copyrightEl = document.querySelector('#copyright-year');
       app.getDate.applyYear(copyrightEl, app.getDate.getYear());
-
-
-      // If Masonry exists on the page, initialize
-      if (null != app.masonry.masonryGridEl) {
-        app.masonry.masonryInit();
-      }
-
     }
   };
   window.app = app;
